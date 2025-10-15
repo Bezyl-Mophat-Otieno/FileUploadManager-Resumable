4a77473ef612a3dd5da1b12183bceff8096b58c5
Bezyl Mophat Otieno <bezylmophatotieno@gmail.com>
2025-10-15 23:39:13 +0300

    This is a simple implementation endpoint where we basically just upload a file with the simple multipart/form-data header

---
+++ FileUploadManager/Controllers/FileUploadController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileUploadManager.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class FileUploadController(IWebHostEnvironment env): ControllerBase
+{
+
+    [HttpPost]
+    [RequestSizeLimit(100_000_000)]
+    public async Task<ActionResult> UploadFile(IFormFile file)
+    {
+        if (file.Length == 0)
+        {
+            return BadRequest("No file uploaded");
+        }
+        
+        var uploadPath = Path.Combine(env.ContentRootPath,"wwwroot", "uploads");
+        if (!Directory.Exists(uploadPath))
+        {
+            Directory.CreateDirectory(uploadPath);
+        }
+
+        var fileName = file.FileName;
+        var newFileName = $"{Guid.NewGuid()}_{fileName}";
+        
+        await  using var stream = new FileStream(Path.Combine(uploadPath, newFileName), FileMode.Create);
+
+        await file.CopyToAsync(stream);
+
+        return Ok(new
+        {
+            fileName = file.FileName,
+            savedAs = newFileName,
+            contentType = file.ContentType,
+            sizeInKBB = Math.Round((file.Length / 1024.0), 2),
+            uploadTime = DateTime.UtcNow
+            
+        });
+
+
+    }
+
+}
